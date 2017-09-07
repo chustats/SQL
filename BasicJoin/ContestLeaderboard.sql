f019d87eeba0c0ccb273b3c90a927b9d5f99f4d6
statspotato <chuyyangelia@gmail.com>
2017-09-07 10:02:01 -0500

    Update and rename ContestLeaderboard to ContestLeaderboard.sql

---
+++ BasicJoin/ContestLeaderboard.sql
@@ -0,0 +1,16 @@
+
+select h.hacker_id, h.name, sum(score) as total_score
+from
+hackers as h inner join
+/* find max_score*/
+(select hacker_id,  max(score) as score from submissions group by hacker_id, challenge_id) as max_score
+
+on h.hacker_id=max_score.hacker_id
+group by h.hacker_id, h.name
+
+/* don't accept hackers with total_score=0 */
+having total_score > 0
+
+/* finally order as required */
+order by total_score desc, h.hacker_id
+;
